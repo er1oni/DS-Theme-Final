9c24d93e9637d337496015a50f40443f687711bf
er1oni <erjongashi_@hotmail.com>
2025-03-18 19:13:56 +0100

    ...

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,1 +1,9 @@
-console.log("testing");
+let dropdown = document.querySelector('.menu'), //ul
+submenu = document.querySelector('.nub-menu'), //ul li a
+buttonclick = document.querySelector('.check-button'), //button
+hamburger = document.querySelector('.menu-icon');
+
+buttonclick.addEventListener('click', () => {
+    dropdown.classList.toogle('show-dropdown');
+    
+} )
